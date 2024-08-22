91644168905a589d48261f8d5d30187b55174c1c
Andrea Echavez <129680910+Echavezandrea@users.noreply.github.com>
2024-08-22 14:24:11 +0800

    views.py

--- views.py
+++ views.py
@@ -1,7 +1,22 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.shortcuts import render
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include ('api.urls'))
-]
+# Create your views here.
+
+def index(request ):
+    return render(request, 'index.html')
+def about(request ):
+    return render(request, 'about.html')
+def news(request ):
+    return render(request, 'news.html')
+def issuances(request ):
+    return render(request, 'issuances.html')
+def qms(request ):
+    return render(request, 'qms.html')
+def procurement(request ):
+    return render(request, 'procurement.html')
+def learningresources(request ):
+    return render(request, 'learningresources.html')
+def research(request ):
+    return render(request, 'research.html')
+def contact(request ):
+    return render(request, 'contact.html')
